811bca125bf95b1db638a3f9b54cd40e729cd0d5
kunfeng327 <3273869414@qq.com>
2025-04-19 16:38:08 +0800

    Initial commit

---
+++ compile_run.bat
@@ -0,0 +1,9 @@
+
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+
+"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.41.34120\bin\Hostx64\x64\cl.exe" /EHsc ex5h6.cpp 
+
+ex5h6.exe
+
+pause
--- ex5h6.cpp
+++ ex5h6.cpp
@@ -2,59 +2,126 @@
 #include<cmath>
 using namespace std;
 
-template <typename T> class MyQueue;//前置声明
-template <typename T> std::ostream & operator<<(std::ostream &, const MyQueue<T> &); 
+template <typename T> class MyQueue; // 前置声明
+template <typename T> std::ostream & operator<<(std::ostream &, const MyQueue<T> &);
 
-template <typename T> 
-class QueueItem
-{
+template <typename T>
+class QueueItem {
 public:
-    QueueItem(const T & t) :item(t), next(0)
-    {} 
+    QueueItem(const T & t) : item(t), next(0) {}
 private:
-    T item; //value stored in this element
-    QueueItem *next; // pointer to next element in the MyQueue
+    T item; // 存储的值
+    QueueItem *next; // 指向下一个元素的指针
 
-    friend class MyQueue<T>;//友元类    
-    //通过友元函数重载<<运算符模板函数，要写上<<后的<Type>
-    friend ostream & operator<< <T> (ostream & os, const MyQueue<T> & q);
+    friend class MyQueue<T>; // 友元类
+    friend ostream & operator<< <T>(ostream & os, const MyQueue<T> & q); // 友元函数
 };
 
-template <typename T> 
-class MyQueue
-{
+template <typename T>
+class MyQueue {
 public:
-    MyQueue() : head(0), tail(0) {} // Empty MyQueue
+    MyQueue() : head(0), tail(0) {} // 空队列
 
-    MyQueue(const MyQueue &Q) //拷贝构造函数
-        :head(0), tail(0) 
-    { CopyElements(Q); };
+    MyQueue(const MyQueue &Q) // 拷贝构造函数
+        : head(0), tail(0) 
+    { 
+        CopyElements(Q); 
+    }
 
-    ~MyQueue() { Destroy(); }
+    ~MyQueue() { Destroy(); } // 析构函数
 
-    MyQueue & operator=(const MyQueue &);
+    MyQueue & operator=(const MyQueue &Q) { // 赋值运算符重载
+        if (this != &Q) {
+            Destroy();
+            CopyElements(Q);
+        }
+        return *this;
+    }
 
-    // return element from head of MyQueue
-    T & Front() { return head->item; }
+    T & Front() { return head->item; } // 返回队列头部元素
     const T & Front() const { return head->item; }
-    void Push(const T &); //add element to back of MyQueue
-    void Pop(); // remove element from head of MyQueue
-    bool Empty() const { return head == 0; }
-    void Display() const;
-private:
-    QueueItem<T> *head;
-    QueueItem<T> *tail;
-    void Destroy(); //delete all the elements
-    void CopyElements(const MyQueue &);
+    void Push(const T &); // 添加元素到队列尾部
+    void Pop(); // 从队列头部移除元素
+    bool Empty() const { return head == 0; } // 判断队列是否为空
+    void Display() const; // 显示队列中的所有元素
 
-    //设置友元函数
-    friend ostream & operator<< <T> (ostream & os, const MyQueue<T> & q);
+private:
+    QueueItem<T> *head; // 队列头部指针
+    QueueItem<T> *tail; // 队列尾部指针
+    void Destroy(); // 删除所有元素
+    void CopyElements(const MyQueue &); // 复制队列中的所有元素
+
+    friend ostream & operator<< <T>(ostream & os, const MyQueue<T> & q); // 友元函数
 };
 
-// 实现这个类模板中的成员函数，然后使用如下所示的main()函数测试这一类模板。
+// 添加元素到队列尾部
+template <typename T>
+void MyQueue<T>::Push(const T &val) {
+    QueueItem<T> *newItem = new QueueItem<T>(val);
+    if (tail) {
+        tail->next = newItem;
+    } else {
+        head = newItem;
+    }
+    tail = newItem;
+}
 
-int main()
-{
+// 从队列头部移除元素
+template <typename T>
+void MyQueue<T>::Pop() {
+    if (head) {
+        QueueItem<T> *temp = head;
+        head = head->next;
+        delete temp;
+        if (!head) {
+            tail = nullptr; // 如果队列为空，更新尾部指针
+        }
+    }
+}
+
+// 显示队列中的所有元素
+template <typename T>
+void MyQueue<T>::Display() const {
+    QueueItem<T> *current = head;
+    while (current) {
+        cout << current->item << " ";
+        current = current->next;
+    }
+}
+
+// 删除所有元素
+template <typename T>
+void MyQueue<T>::Destroy() {
+    while (head) {
+        Pop();
+    }
+}
+
+// 复制队列中的所有元素
+template <typename T>
+void MyQueue<T>::CopyElements(const MyQueue &Q) {
+    QueueItem<T> *current = Q.head;
+    while (current) {
+        Push(current->item);
+        current = current->next;
+    }
+}
+
+// 重载 << 运算符
+template <typename T>
+ostream & operator<<(ostream &os, const MyQueue<T> &q) {
+    QueueItem<T> *current = q.head;
+    os << "< ";
+    while (current) {
+        os << current->item << " ";
+        current = current->next;
+    }
+    os << ">";
+    return os;
+}
+
+// 测试代码
+int main() {
     MyQueue<int> qi;
     qi.Push(1);
     qi.Push(2);
@@ -63,17 +130,17 @@ int main()
     qi.Push(5);
     qi.Pop();
     qi.Display();
-    cout<<"\n";
-    cout<<qi;
-    cout<<endl;
+    cout << "\n";
+    cout << qi;
+    cout << endl;
 
     MyQueue<int> qi2(qi);
     qi2.Display();
-    cout<<endl;
+    cout << endl;
 
     MyQueue<int> qi3;
     qi3 = qi;
-    cout<<qi3;
+    cout << qi3;
 
     return 0;
 }
